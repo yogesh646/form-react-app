bd60d4d016539b31c90d0743b2773172581ea049
yogesh646 <blee44921@gmail.com>
2024-10-28 12:39:58 +0530

    initial changes

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import  { FormField } from './FormField';
+import Form from './Form';
+
+
+const App: React.FC = () => {
+
+
+  return (
+    <div>
+      <h1>Dynamic Tsx</h1>
+   <Form />
+      {/* <button onClick={check}>check</button> */}
+    </div>
+  );
+};
+
+export default App;
